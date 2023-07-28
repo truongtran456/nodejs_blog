644e024dd6aeedda34dc2d3ea8b135b05d7d6e8a
Quoc_Truong <98575368+truongtran456@users.noreply.github.com>
2023-07-28 22:08:29 +0700

    Update me.js

--- src/routes/me.js
+++ src/routes/me.js
@@ -6,6 +6,4 @@ const meController= require('../app/controllers/MeController');
 // newsController.index
 router.get('/stored/courses',meController.storedCourses)//den file storedCourses ben controller
 router.get('/trash/courses',meController.trashCourses)//den file storedCourses ben controller
-
-    
 module.exports=router;
